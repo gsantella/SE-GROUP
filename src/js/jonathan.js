a1a79685564359faf708d7ed83c673c7865a3528
Guido Santella <gsantella@southhills.edu>
2019-09-11 08:49:29 -0400

    test experiment again

---
+++ src/js/jonathan.js
@@ -0,0 +1,12 @@
+const addTwoNumbers = (num1, num2) => {
+    return 0;
+}
+
+const averageThreeNumbers = (num1, num2, num3) => {
+    return 0;
+}
+
+module.exports = {
+    addTwoNumbers,
+    averageThreeNumbers
+}
--- tests/script-library.test.js
+++ tests/script-library.test.js
@@ -1,9 +1,34 @@
 const lib = require('../src/js/script-library.js');
+const libJonathan = require('../src/js/jonathan.js');
+const libJason = require('../src/js/jason.js');
+const libWinter = require('../src/js/winter.js');
 
-test('CToF', () => {
-  expect(lib.celsiusToFahrenheit(100)).toBe(888);
+describe('Original Tests', () => {
+  test('CToF', () => {
+    expect(lib.celsiusToFahrenheit(100)).toBe(888);
+  });
+
+  test('FtoC1', () => {
+    expect(lib.fahrenheitToCelsius(212)).toBe(999);
+  });
+
+  test('FtoC2', () => {
+    expect(lib.fahrenheitToCelsius(32)).toBe(999);
+  });
 });
 
-test('FtoC', () => {
-  expect(lib.fahrenheitToCelsius(212)).toBe(999);
+describe('All addTwoNumbers', () => {
+  test('addTwoNumbers', () => { 
+    expect(libJonathan.addTwoNumbers(1,2)).toBe(3);
+    expect(libJason.addTwoNumbers(1,2)).toBe(3);
+    expect(libWinter.addTwoNumbers(1,2)).toBe(3);
+  });
+});
+
+describe('All averageThreeNumbers', () => {
+  test('averageThreeNumbers', () => { 
+    expect(libJonathan.averageThreeNumbers(1,2,3)).toBe(2);
+    expect(libJason.averageThreeNumbers(1,2,3)).toBe(3);
+    expect(libWinter.averageThreeNumbers(1,2,3)).toBe(3);
+  });
 });
