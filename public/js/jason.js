d4030f13de6989d54e765c746efc34d85a0135ad
Guido <gsantella@southhills.edu>
2019-09-12 06:48:51 -0400

    few updates for building and testing

---
+++ public/js/jason.js
@@ -0,0 +1,12 @@
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2;
+}
+
+const averageThreeNumbers = (num1, num2, num3) => {
+    return ((num1 + num2 + num3) / 3);
+}
+
+module.exports = {
+    addTwoNumbers,
+    averageThreeNumbers
+}
---
+++ public/js/script-library.js
@@ -0,0 +1,12 @@
+let fahrenheitToCelsius = (temp) => {
+    return 999;
+}
+
+let celsiusToFahrenheit = (temp) => {
+    return 888;
+}
+
+module.exports = {
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
+}
