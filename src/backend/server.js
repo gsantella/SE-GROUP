6bc7a60d10387ff598a18d7e789936ae7db5612f
Guido Santella <gsantella@southhills.edu>
2019-09-18 08:51:55 -0400

    api experiment

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,5 @@
-import express from 'express';
+// import express from 'express';
+const express = require('express');
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,19 @@ const data = {
   status: 'success',
 };
 
+// routes
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api', (req, res) => res.json(data));
+app.get('/temp', (req, res) => {
+  if (req.query.f) {
+    const { f } = req.query;
+    const c = (f - 32) * (5 / 9);
+    data.status = 'success';
+    data.result = c;
+  } else {
+    data.status = 'fail';
+  }
+  res.json(data);
+});
 
 app.listen(port);
