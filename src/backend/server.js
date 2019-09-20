2c10aa70014d6bcbd7bc0b63aa8bdc2cecba4052
Guido Santella <gsantella@southhills.edu>
2019-09-20 08:05:07 -0400

    class work

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,13 @@
+/* eslint-disable quotes */
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 // routes
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/color', (req, res) => res.json({ color: Math.floor(Math.random() * 10) + 1 }));
 app.get('/api', (req, res) => res.json({ status: 'success' }));
 app.get('/temp', (req, res) => {
   const data = {};
@@ -18,5 +21,7 @@ app.get('/temp', (req, res) => {
   }
   res.json(data);
 });
+
+app.use(cors());
 
 app.listen(port);
--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -19,3 +19,14 @@ function convertCtoF(num) {
   const final = celsiusToFahrenheit(num);
   document.getElementById('f2').value = final;
 }
+
+// Dynamic API Example
+document.querySelector('button').addEventListener('click', () => {
+  alert('Howdy!');
+  fetch('http://localhost:3000/color')
+    .then((resp) => resp.json())
+    .then((json) => {
+      document.querySelector('#dynamic-selector').innerHTML = json.color;
+    })
+    .then(alert('I\'m done!'));
+});
