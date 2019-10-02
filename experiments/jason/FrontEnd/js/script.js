5c5403f7c0ead64a12afaa6ce40a0d51bc1c6cfd
Jason S <jas.schopp@gmail.com>
2019-10-02 08:16:27 -0400

    send data to API

--- experiments/jason/FrontEnd/js/script.js
+++ experiments/jason/FrontEnd/js/script.js
@@ -1,5 +1,6 @@
 
 document.getElementById('autoEmailer').addEventListener('click', () => {
+  const url = 'https://api.myjson.com/bins';
   const data = {};
   data.recieverName = document.getElementById('recieverName').value;
   data.recieverEmail = document.getElementById('recieverEmail').value;
@@ -9,4 +10,20 @@ document.getElementById('autoEmailer').addEventListener('click', () => {
   data.email = document.getElementById('email').value;
   data.msg = document.getElementById('msg').value;
   console.log(data);
+  async function sendData() {
+    try {
+      const response = await fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const json = await response.json();
+      console.log('Success:', JSON.stringify(json));
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  sendData();
 });
